285d3903f0b0d07877345f350fb83367e3ac54cb
Lucian Musat <l.musat@activevideo.com>
2018-01-31 13:28:51 +0100

    hello vendingmachine

---
+++ src/main/java/at/lucianmus/vendingmachine/Main.java
@@ -0,0 +1,13 @@
+package at.lucianmus.vendingmachine;
+
+public class Main {
+   public static void main(String[] args) {
+       Main m = new Main();
+       m.hello();
+   }
+
+   public boolean hello() {
+       System.out.println("hello vendingmachine");
+       return true;
+   }
+}
---
+++ src/test/java/at/lucianmus/vendingmachine/MainTest.java
@@ -0,0 +1,11 @@
+package at.lucianmus.vendingmachine;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+   @Test public void testMain() {
+       Main m = new Main();
+       assertTrue("Main::hello should return 'true'", m.hello());
+   }
+}
